da079b208affa1b52544ca893d12bd82fc7cb69d
Sasha Marks <sasha@outsight.com.au>
2025-08-26 13:34:05 +1000

    TouchGFX change and flashing green LED

--- Appli/TouchGFX/gui/src/model/Model.cpp
+++ Appli/TouchGFX/gui/src/model/Model.cpp
@@ -1,6 +1,8 @@
 #include <gui/model/Model.hpp>
 #include <gui/model/ModelListener.hpp>
 
+#include <../../Core/Inc/main.h>
+
 Model::Model() : modelListener(0)
 {
 
@@ -8,5 +10,6 @@ Model::Model() : modelListener(0)
 
 void Model::tick()
 {
-
+    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+    HAL_Delay(80);
 }
